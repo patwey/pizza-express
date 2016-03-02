1713f95c4d865561cb72411f391ed673e4510879
Pat Wey <patrickwey@gmail.com>
2016-03-02 16:34:37 -0700

    Serving static assets

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
+app.use(express.static('public'));
 
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Pizza Express';
 
 app.get('/', (request, response) => {
-  response.send(app.locals.title);
-});
-
-app.listen(app.get('port'), () => {
-  console.log(`${app.locals.title} is running on ${app.get('port')}.`);
+  response.sendFile(path.join(__dirname, '/static/index.html'));
 });
 
 if (!module.parent) {
