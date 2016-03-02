6904ae15a82f2ded5317f378ed581d9ef06435a2
Pat Wey <patrickwey@gmail.com>
2016-03-02 16:25:14 -0700

    Request defaults

--- test/server-test.js
+++ test/server-test.js
@@ -5,9 +5,14 @@ const request = require('request');
 describe('Server', () => {
   before(done => {
     this.port = 9876;
+
     this.server = app.listen(this.port, (err, result) => {
       if (err) { return done(err); }
         done();
+    });
+
+    this.request = request.defaults({
+      baseUrl: 'http://localhost:9876/'
     });
   });
 
@@ -21,9 +26,20 @@ describe('Server', () => {
 
   describe('GET /', () => {
     it('should return a 200', (done) => {
-      request.get('http://localhost:9876', (error, response) => {
+      this.request.get('/', (error, response) => {
         if (error) { done(error); }
         assert.equal(response.statusCode, 200);
+        done();
+      });
+    });
+
+    it('should have a body with the name of the application', (done) => {
+      var title = app.locals.title;
+
+      this.request.get('/', (error, response) => {
+        if (error) { done(error); }
+        assert(response.body.includes(title),
+               `"${response.body}" does not include "${title}".`);
         done();
       });
     });
