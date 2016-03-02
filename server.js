31f6173814cafee16c11c593b0db3718f8ac4f8d
Pat Wey <patrickwey@gmail.com>
2016-03-02 15:59:59 -0700

    Making requests to our server

--- server.js
+++ server.js
@@ -11,3 +11,11 @@ app.get('/', (request, response) => {
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
 });
+
+if (!module.parent) {
+  app.listen(app.get('port'), () => {
+    console.log(`${app.locals.title} is running on ${app.get('port')}.`);
+  });
+}
+
+module.exports = app;
